c7eaaf0dc8eed841f6d8cbc55b2cb954260ebb7c
unknown <shayan.shaikh@reveation.io>
2022-05-06 12:31:00 +0530

    Update: marketplace code

---
+++ contracts/marketPlace.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+
+contract marketPlace is ERC1155Holder {}
